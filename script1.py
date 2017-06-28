04193fa3f7932b5ac32c4d27b25daae1fa4f7dc8
Weilin Luo <weilin.william.luo@gmail.com>
2017-06-27 20:58:17 -0700

    Add script1.py

---
+++ script1.py
@@ -0,0 +1,7 @@
+# A first python script
+import sys          # Load a library module
+print(sys.platform)
+print(2 ** 100)     # raise 2 to a power
+x = 'Spam!'
+print(x * 8)        # string repetition
+
